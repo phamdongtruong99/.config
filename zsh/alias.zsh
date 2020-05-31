6189e3b35520f5d147c8f82158d19892e7c8361a
phamtruong <truongpd.doublenine@gmail.com>
2020-06-01 00:02:15 +0700

    update

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -17,18 +17,43 @@ alias devdocs='open -a "Google Chrome" https://codesandbox.io/'
 alias ghub="open https://github.com/phamdongtruong99"
 
 # alias node : 
-
 alias nv10="nvm use v10"
 alias nv8="nvm use v8"
-
 alias nps="npm start"
 
-# alias git
+# Git shortcut
 alias pulldev="git pull origin dev"
 alias rbdev="git rebase dev"
 alias gamend="git commit --amend"
+alias g='git'
+alias gca="git add . && git commit --amend"
+alias gst="git status"
+alias gls="git log --oneline"
+alias gll="git log --graph"
+alias grl="git reflog"
+alias gci="git add . && git commit -m "
+alias gcf="git add . && git commit --fixup HEAD"
+alias grbi="git rebase -i --autosquash "
+alias grbm="git pull origin master --rebase"
+alias grbd="git pull origin develop --rebase"
+alias grb="git rebase"
+alias gbr="git branch"
+alias gco="git checkout"
+alias grbc="git add . && git rebase --continue"
+alias gpf="git push -f"
+alias gp="git push"
+alias gpr="git rev-parse --abbrev-ref HEAD | xargs -I {} git push -u origin {}"
+alias gtd="git tag -d "
+alias gprb="git pull --rebase"
+alias gcod="git fetch origin && git checkout origin/develop"
+alias gcom="git fetch origin && git checkout origin/master"
+alias gd="git diff"
+alias grs="git reset"
+alias grsh="git reset --hard"
+alias grss="git reset --soft"
 
 #alias others
+
 
 alias hc="history"
 alias tmux="tmux -u"
